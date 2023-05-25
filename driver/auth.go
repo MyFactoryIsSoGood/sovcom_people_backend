85172191c9608ad2852a1f6ce9939f1511f953f7
Rustam <myfactoryisgood@gmail.com>
2023-05-25 23:50:49 +0500

    fix

--- driver/auth.go
+++ driver/auth.go
@@ -7,7 +7,7 @@ import (
 
 func CheckCredentials(phone, pass string) (bool, models.User) {
 	var user models.User
-	db.DB.Model(&models.User{}).Select("WHERE phone = ? AND password = ?", phone, pass).First(&user)
+	db.DB.Model(&models.User{}).Where("phone = ? AND password = ?", phone, pass).First(&user)
 
 	if user.Phone != "" {
 		return true, user
