04d9c6d8279473cca9f0296ffc4cc17e8b9279a1
Rustam <myfactoryisgood@gmail.com>
2023-05-25 22:54:14 +0500

    fix

--- db/db.go
+++ db/db.go
@@ -12,10 +12,11 @@ var DB *gorm.DB
 
 func Connect() error {
 	//Docker config:
-	args := fmt.Sprintf("user=%v dbname=%v password=%v sslmode=disable host=localhost port=5432",
+	args := fmt.Sprintf("user=%v dbname=%v password=%v sslmode=disable host=%v port=5432",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PASS"))
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"))
 
 	db, err := gorm.Open("postgres", args)
 
